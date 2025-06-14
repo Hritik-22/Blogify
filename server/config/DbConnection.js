37d9e2cd3e7d95f6a7897a99cd2562012c303ad4
Ritik dubey <95172948+Hritik-22@users.noreply.github.com>
2025-06-14 23:29:36 +0530

    DbConnection.js

--- server/config/DbConnection.js
+++ server/config/DbConnection.js
@@ -3,10 +3,9 @@ dotenv.config();
 // config/DbConnection.js
 import { Sequelize } from "sequelize";
 
-
-
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+     port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false,
 });
