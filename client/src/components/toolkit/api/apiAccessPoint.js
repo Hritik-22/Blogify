a73754028d808600c039ac82d03db0b9a50698f5
Ritik dubey <95172948+Hritik-22@users.noreply.github.com>
2025-06-15 10:26:01 +0530

    Update apiAccessPoint.js

--- client/src/components/toolkit/api/apiAccessPoint.js
+++ client/src/components/toolkit/api/apiAccessPoint.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const url = import.meta.env.VITE_BACKEND_URL;
-
+console.log(url);
 const api = axios.create({
     baseURL: url || "http://localhost:5001/api/v1",
     withCredentials: true,
